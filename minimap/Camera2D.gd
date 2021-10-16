0d97c4457f355bb79c1d3aa9f953c02870088e3b
nazgum <nazgum@gmail.com>
2021-10-16 13:59:42 -0400

    simplify camera

--- minimap/Camera2D.gd
+++ minimap/Camera2D.gd
@@ -5,6 +5,23 @@ var rate   = 4
 var cutoff = 4
 
 onready var player = get_node("../Player")
+onready var caves  = get_node("../Caves")
+
+
+func _ready():
+	var rect      = caves.get_used_rect()
+	var tile_size = caves.tile_size
+
+	# set the camera limits to be the size of the caves
+	limit_left   = rect.position.x * tile_size
+	limit_right  = rect.end.x * tile_size
+	limit_top    = rect.position.y * tile_size
+	limit_bottom = rect.end.y * tile_size
+
+	# start the camera at the center of the caves
+	position.x = rect.position.x + (rect.size.x/2 * tile_size)
+	position.y = rect.position.y + (rect.size.y/2 * tile_size)
+
 
 
 func _process(delta):
